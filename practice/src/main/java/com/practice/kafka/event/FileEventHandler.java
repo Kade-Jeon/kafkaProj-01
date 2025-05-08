e1f3a723b427f76fc2372378b71d3177cd4c0dcc
kade <zsc0248@gmail.com>
2025-05-08 21:24:27 +0900

    ~ section9. 103

---
+++ practice/src/main/java/com/practice/kafka/event/FileEventHandler.java
@@ -0,0 +1,75 @@
+package com.practice.kafka.event;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+
+public class FileEventHandler implements EventHandler {
+
+    public static final Logger logger = LoggerFactory.getLogger(FileEventHandler.class);
+    private KafkaProducer<String, String> kafkaProducer;
+    private String topicName;
+
+    private boolean isSync;
+
+    public FileEventHandler(KafkaProducer<String, String> kafkaProducer, String topicName, boolean isSync) {
+        this.kafkaProducer = kafkaProducer;
+        this.topicName = topicName;
+        this.isSync = isSync;
+    }
+
+    @Override
+    public void onMessage(MessageEvent messageEvent) throws InterruptedException, ExecutionException {
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(this.topicName, messageEvent.key, messageEvent.value);
+
+        if (this.isSync) {
+            RecordMetadata metadata = this.kafkaProducer.send(producerRecord).get();
+            logger.info("""
+                            ##### record metadata received #####
+                            partition: {}
+                            offset: {}
+                            timestamp: {}
+                            """,
+                    metadata.partition(), metadata.offset(), metadata.timestamp());
+        } else {
+            this.kafkaProducer.send(producerRecord, (metadata, exception) -> {
+                if (exception != null) {
+                    logger.info("""
+                                   \n##### record metadata received #####
+                                    partition: {}
+                                    offset: {}
+                                    timestamp: {}
+                                    """,
+                            metadata.partition(), metadata.offset(), metadata.timestamp());
+                } else {
+                    logger.error(exception.getMessage());
+                }
+            });
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String topicName = "file-topic";
+
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+        boolean isSync = true;
+
+        FileEventHandler fileEventHandler = new FileEventHandler(kafkaProducer, topicName, isSync);
+        MessageEvent messageEvent = new MessageEvent("key001", "this is test message");
+        fileEventHandler.onMessage(messageEvent);
+    }
+
+}
---
+++ practice/src/main/java/com/practice/kafka/producer/FileAppendProducer.java
@@ -0,0 +1,45 @@
+package com.practice.kafka.producer;
+
+import com.practice.kafka.event.EventHandler;
+import com.practice.kafka.event.FileEventHandler;
+import com.practice.kafka.event.FileEventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Properties;
+
+public class FileAppendProducer {
+    public static final Logger logger = LoggerFactory.getLogger(FileAppendProducer.class);
+    public static void main(String[] args) {
+        String topicName = "file-topic";
+        //String filePath = "C:\\Users\\crinity\\Documents\\study\\kafka-core\\kafkaProj-01\\practice\\src\\main\\resources\\pizza_sample.txt"; // Window
+        String filePath = "/Users/kade/Documents/kafkaProj-01/practice/src/main/resources/pizza_append.txt"; // MAC
+
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // KafkaProducer 객체 생성 <키, 밸류>
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+        boolean isSync = true;
+
+        File file = new File(filePath);
+        EventHandler eventHandler = new FileEventHandler(kafkaProducer, topicName, isSync);
+        FileEventSource fileEventSource = new FileEventSource(1000, file, eventHandler);
+        Thread fileEventSourceThread = new Thread(fileEventSource);
+        fileEventSourceThread.start();
+
+        try {
+            fileEventSourceThread.join();
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage());
+        } finally {
+            kafkaProducer.close();;kafkaProducer.close();
+        }
+    }
+}
